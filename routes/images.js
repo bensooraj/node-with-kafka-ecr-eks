1954aa3a36a95c3d758ce0d9916f5b94c79871d3
Ben Sooraj <bensoorajm@gmail.com>
2019-09-14 20:33:15 +0530

    image upload fine tune

--- routes/images.js
+++ routes/images.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const shortid = require('shortid');
+const moment = require('moment');
 
 const multer = require('multer');
 const imageUploader = multer({
