e20648ed430dff9c460bc07d197fd0aca72ffc79
Ben Sooraj <bensoorajm@gmail.com>
2019-10-20 16:12:07 +0530

    docker compose update to use to puplic repo

--- useful_scripts.sh
+++ useful_scripts.sh
@@ -3,9 +3,11 @@ docker rm $(docker ps -a -f status=exited -q)
 
 # Build the Node.js API + Kafka Producer Image
 docker build -t nodejs-api-kafka-producer-app:v1 .
+docker build -t bensooraj/demo-node-producer:v1 .
 
 # Build the Node.js Kafka Consumer Image
 docker build -t nodejs-kafka-consumer-app:v1 .
+docker build -t bensooraj/demo-node-consumer:v1 .
 
 # Restart one Docker service by updating the image
 docker-compose -f docker-compose/docker-compose.yml up -d --no-deps nodejs-consumer-service
